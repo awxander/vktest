39d0663d976b2d7613a78fef919df66ca2fb5457
Alexander Tsibin <sashok.tsibin@gmail.com>
2023-05-05 01:37:25 +0700

    добавлены кнопки на MainActivity

--- app/src/main/java/com/example/vktest/MainActivity.kt
+++ app/src/main/java/com/example/vktest/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.vktest
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.example.vktest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var _binding : ActivityMainBinding? = null
+    private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (granted) {
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.permission_received),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +37,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initRecyclerView()
+        initListeners()
     }
 
-    private fun initRecyclerView(){
+    private fun initListeners(){
+        binding.apply {
+            loadFilesButton.setOnClickListener {
+                checkPermission()
+                //TODO вызов ViewModel с loadFiles
+            }
+        }
+    }
+
+    private fun initRecyclerView() {
 
     }
+
+
+    private fun checkPermission() {
+        if (!isPermissionGranted()) {
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    private fun isPermissionGranted(): Boolean =
+        PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_CONTACTS
+        )
 }
